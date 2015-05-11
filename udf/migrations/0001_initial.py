ace940e3e6ae566c640374a70906e0c6b2a6edf5
Luis Del Giudice <luis.dg19@gmail.com>
2015-05-11 17:46:04 -0400

    Queryset now builds custom fields

---
+++ udf/migrations/0001_initial.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='UDF',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=50)),
+                ('type', models.CharField(max_length=100)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+            ],
+        ),
+    ]
--- udf/models.py
+++ udf/models.py
@@ -1,7 +1,66 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from jsonfield import JSONField
+from django.db.models.query import ModelIterator
+
+
+class UDF(models.Model):
+
+    def __str__(self):
+        return "%s's %s" % (self.content_type, self.name)
+
+    content_type = models.ForeignKey(ContentType)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=100)
+
+
+class UDFIterator(ModelIterator):
+
+    def __iter__(self):
+        for obj in super(UDFIterator, self).__iter__():
+            setattr(obj, 'this', 'that')
+            yield obj
+
+
+class UDFQuerySet(models.QuerySet):
+
+    def __init__(self, *args, **kwargs):
+        super(UDFQuerySet, self).__init__(*args, **kwargs)
+
+    def iterator(self):
+        return UDFIterator(self)
+
+
+class UDFModelManager(models.Manager):
+
+    def get_queryset(self):
+        return UDFQuerySet(self.model, using=self._db, hints=self._hints)
+
+    def create(self, *args, **kwargs):
+        custom_fields = UDF.objects.filter(
+            content_type=ContentType.objects.get_for_model(self.model))
+
+        udfs = {}
+        for field in custom_fields:
+            udfs[field.name] = kwargs[field.name]
+
+        kwargs['udfs'] = udfs
+        return super(UDFModelManager, self).create(*args, **kwargs)
 
 
 class UDFModel(models.Model):
 
+    udfs = JSONField()
+    objects = UDFModelManager()
+
     class Meta:
         abstract = True
+
+    def __init__(self, *args, **kwargs):
+        custom_fields = UDF.objects.filter(
+            content_type=ContentType.objects.get_for_model(self.__class__))
+
+        for field in custom_fields:
+            setattr(self, field.name, kwargs.pop(field.name, None))
+
+        return super(UDFModel, self).__init__(*args, **kwargs)
